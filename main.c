ed51d823e9ccf17140dad2a4ed0f66d9c15eee97
friengineer <174277232+friengineer@users.noreply.github.com>
2024-07-26 20:13:32 +0100

    Tidy up code

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main( int argc, char **argv )
     cl_command_queue queue = clCreateCommandQueue( context, device, 0, &status );
 
     // Allocate memory for the matrix
-    float *hostMatrix = (float*) malloc( nRows*nCols*sizeof(float) );
+    float *hostMatrix = (float*) malloc( nRows * nCols * sizeof(float) );
 
     // Fill the matrix with random values and display
     fillMatrix( hostMatrix, nRows, nCols );
@@ -26,8 +26,8 @@ int main( int argc, char **argv )
 
     // Allocate memory for the host's matrix and the resulting transposed matrix on
     // the device
-    cl_mem deviceMatrix = clCreateBuffer( context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, nRows*nCols*sizeof(float), hostMatrix, &status );
-  	cl_mem deviceTransposed = clCreateBuffer( context, CL_MEM_WRITE_ONLY, nRows*nCols*sizeof(float), NULL, &status );
+    cl_mem deviceMatrix = clCreateBuffer( context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, nRows * nCols * sizeof(float), hostMatrix, &status );
+  	cl_mem deviceTransposed = clCreateBuffer( context, CL_MEM_WRITE_ONLY, nRows * nCols * sizeof(float), NULL, &status );
 
     int deviceRows = nRows, deviceCols = nCols;
 
@@ -42,22 +42,22 @@ int main( int argc, char **argv )
 
     // Set the global problem size
     size_t indexSpaceSize[1];
-    indexSpaceSize[0] = nRows*nCols;
+    indexSpaceSize[0] = nRows * nCols;
 
     // Put the kernel onto the command queue letting the compiler determine a suitable
     // work group size automatically
     status = clEnqueueNDRangeKernel( queue, kernel, 1, NULL, indexSpaceSize, NULL, 0, NULL, NULL );
 
-  	if( status != CL_SUCCESS )
+  	if ( status != CL_SUCCESS )
   	{
   		printf( "Failure enqueuing kernel: Error %d.\n", status );
   		return EXIT_FAILURE;
   	}
 
     // Read the result from the device to the host
-    status = clEnqueueReadBuffer( queue, deviceTransposed, CL_TRUE, 0, nRows*nCols*sizeof(float), hostMatrix, 0, NULL, NULL );
+    status = clEnqueueReadBuffer( queue, deviceTransposed, CL_TRUE, 0, nRows * nCols * sizeof(float), hostMatrix, 0, NULL, NULL );
 
-  	if( status != CL_SUCCESS )
+  	if ( status != CL_SUCCESS )
   	{
   		printf( "Could not copy device data to host: Error %d.\n", status );
   		return EXIT_FAILURE;
